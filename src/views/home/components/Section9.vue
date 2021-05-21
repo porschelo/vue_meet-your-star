34822abed8b4ebfeecff3dbacdbce8c9a35aeabc
porschelo <leavetwin@gmail.com>
2021-05-22 01:01:09 +0800

    add swiper

--- src/views/home/components/Section9.vue
+++ src/views/home/components/Section9.vue
@@ -1,69 +1,282 @@
 <template>
     <div class="i-scetion9">
-        <!-- <swiper
-            :slidesPerView="1"
-            :spaceBetween="10"
-            :pagination="{
-                clickable: true,
-            }"
-            :breakpoints="{
-                '640': {
-                    slidesPerView: 1,
-                    spaceBetween: 20,
-                },
-                '768': {
-                    slidesPerView: 2,
-                    spaceBetween: 40,
-                },
-                '1024': {
-                    slidesPerView: 3,
-                    spaceBetween: 50,
-                },
-            }"
-            class="mySwiper"
-        >
-            <swiper-slide>Slide 1</swiper-slide
-            ><swiper-slide>Slide 2</swiper-slide
-            ><swiper-slide>Slide 3</swiper-slide
-            ><swiper-slide>Slide 4</swiper-slide
-            ><swiper-slide>Slide 5</swiper-slide
-            ><swiper-slide>Slide 6</swiper-slide
-            ><swiper-slide>Slide 7</swiper-slide
-            ><swiper-slide>Slide 8</swiper-slide
-            ><swiper-slide>Slide 9</swiper-slide>
+        <!-- <swiper class="swiper" :options="swiperOption">
+           
         </swiper> -->
+
+        <div class="section9_boxTop">
+            <!-- box1 -->
+            <div class="unstore_title">
+                <div class="unstore_title-c">
+                    <img src="images/store/uv_icone.png" alt="" />
+                    <h1>每日運勢</h1>
+                    <img src="images/store/uv_icone.png" alt="" />
+                </div>
+                <div class="unstore_title-e">
+                    <h4>— Universe Store —</h4>
+                </div>
+            </div>
+            <!-- Swiper -->
+            <div class="index_swiper crs">
+                <div class="index_prbtn">
+                    <a href="">
+                        <img src="/images/index/prbtn.png" alt="" />
+                    </a>
+                </div>
+                <div class="swiper-container mySwiper gap">
+                    <swiper class="swiper-wrapper" :options="swiperOption">
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="sing_info1">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing1.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察
+                                        力強，如果是從事創意
+                                        工作者在今天能好好發 揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="sing_info2">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing2.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察力強，如果是從事創意
+                                        工作者在今天能好好發揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="product_info3">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing3.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察
+                                        力強，如果是從事創意
+                                        工作者在今天能好好發 揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="product_info4">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing1.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察
+                                        力強，如果是從事創意
+                                        工作者在今天能好好發 揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+                    </swiper>
+                    <!-- <div class="swiper-pagination"></div> -->
+                </div>
+
+                <div class="index_nextbtn">
+                    <a href="">
+                        <img src="/images/index/nxbtn.png" alt="" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="section9_boxButtom">
+            <!-- box1 -->
+            <div class="unstore_title">
+                <div class="unstore_title-c">
+                    <img src="images/store/uv_icone.png" alt="" />
+                    <h1>每日運勢</h1>
+                    <img src="images/store/uv_icone.png" alt="" />
+                </div>
+                <div class="unstore_title-e">
+                    <h4>— Universe Store —</h4>
+                </div>
+            </div>
+            <!-- Swiper -->
+            <div class="index_swiper crs">
+                <div class="index_prbtn2">
+                    <a href="">
+                        <img src="/images/index/prbtn.png" alt="" />
+                    </a>
+                </div>
+                <div class="swiper-container mySwiper gap">
+                    <swiper class="swiper-wrapper" :options="swiperOption2">
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="sing_info1">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing1.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察
+                                        力強，如果是從事創意
+                                        工作者在今天能好好發 揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="sing_info2">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing2.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察力強，如果是從事創意
+                                        工作者在今天能好好發揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="product_info3">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing3.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察
+                                        力強，如果是從事創意
+                                        工作者在今天能好好發 揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+
+                        <swiper-slide>
+                            <a href="starsign.html">
+                                <div class="product_info4">
+                                    <img
+                                        class="info_img"
+                                        src="/images/index/info_Img/sing1.svg"
+                                        alt=""
+                                    />
+                                    <h3 class="inner_text">
+                                        今天的你想像力、洞察
+                                        力強，如果是從事創意
+                                        工作者在今天能好好發 揮，但也要避免...
+                                    </h3>
+                                </div>
+                            </a>
+                        </swiper-slide>
+                    </swiper>
+                    <!-- <div class="swiper-pagination"></div> -->
+                </div>
+
+                <div class="index_nextbtn2">
+                    <a href="">
+                        <img src="/images/index/nxbtn.png" alt="" />
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-//
 <script>
-// // Import Swiper Vue.js components
-// // import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+// import style (<= Swiper 5.x)
+import 'swiper/dist/css/swiper.css';
 
-// import 'swiper/components/pagination/pagination.min.css';
+export default {
+    name: 'carrousel',
 
-// // import Swiper core and required modules
-// // import SwiperCore, { Pagination } from 'swiper/core';
+    data() {
+        return {
+            swiperOption: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                loop: true,
 
-// // install Swiper modules
-// SwiperCore.use([Pagination]);
+                navigation: {
+                    nextEl: '.index_nextbtn',
+                    prevEl: '.index_prbtn',
+                },
+                breakpoints: {
+                    // 1024: {
+                    //     slidesPerView: 3,
+                    //     spaceBetween: 40,
+                    // },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    640: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                },
+            },
+            swiperOption2: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                loop: true,
 
-// export default {
-//     components: {
-//         Swiper,
-//         SwiperSlide,
-//     },
-//     data() {
-//         return {};
-//     },
-//     methods: {},
-// };
-//
+                navigation: {
+                    nextEl: '.index_nextbtn2',
+                    prevEl: '.index_prbtn2',
+                },
+                breakpoints: {
+                    // 1024: {
+                    //     slidesPerView: 3,
+                    //     spaceBetween: 40,
+                    // },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    640: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                },
+            },
+        };
+    },
+
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+};
 </script>
 
 <style lang="scss">
 @import '../../../scss/mixins.scss';
+@import '../../../scss/color.scss';
+@import '../../../scss/var.scss';
 .i-scetion9 {
+    position: relative;
     width: 100%;
     height: 2270px;
     background-image: url('/images/index/buttom_bg.svg');
@@ -71,54 +284,183 @@
     background-position: center center;
     background-size: cover;
 
-    // #app {
-    //     height: 100%;
-    // }
-    // html,
-    // body {
-    //     position: relative;
-    //     height: 100%;
-    // }
+    .section9_boxTop {
+        padding-top: 650px;
+        .unstore_title {
+            // border: 1px solid red;
+            color: $colorT;
+            padding: 80px * 0.7 0 80px;
+            &-c {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                img {
+                    width: 63px * 0.7;
+                    height: 43px * 0.7;
+                    margin-bottom: 5px;
+                }
+                h1 {
+                    font-size: $h1;
+                    padding: 5px 20px 10px;
+                }
+            }
+            &-e {
+                text-align: center;
+                font-size: $p1;
+            }
+        }
 
-    // body {
-    //     background: #eee;
-    //     font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-    //     font-size: 14px;
-    //     color: #000;
-    //     margin: 0;
-    //     padding: 0;
-    // }
+        // ------ Swiper  ------
+        .index_swiper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 2%;
+            .index_prbtn {
+                margin-right: 15px;
+            }
+            .index_nextbtn {
+                margin-left: 15px;
+            }
+        }
+        .swiper-container {
+            width: 85%;
+            height: 100%;
 
-    // .swiper-container {
-    //     width: 100%;
-    //     height: 100%;
-    // }
+            .swiper-slide {
+                padding: 10px;
+                text-align: center;
+                font-size: 18px;
 
-    // .swiper-slide {
-    //     text-align: center;
-    //     font-size: 18px;
-    //     background: #fff;
+                background-color: rgba(218, 199, 199, 0.6);
+                height: 400px;
+                width: 400px;
+                border: 5px solid rgb(65, 121, 173);
+                // border-radius: 55%;
 
-    //     /* Center slide text vertically */
-    //     display: -webkit-box;
-    //     display: -ms-flexbox;
-    //     display: -webkit-flex;
-    //     display: flex;
-    //     -webkit-box-pack: center;
-    //     -ms-flex-pack: center;
-    //     -webkit-justify-content: center;
-    //     justify-content: center;
-    //     -webkit-box-align: center;
-    //     -ms-flex-align: center;
-    //     -webkit-align-items: center;
-    //     align-items: center;
-    // }
+                /* Center slide text vertically */
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: -webkit-flex;
+                display: flex;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                -webkit-justify-content: center;
+                justify-content: center;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                -webkit-align-items: center;
+                align-items: center;
 
-    // .swiper-slide img {
-    //     display: block;
-    //     width: 100%;
-    //     height: 100%;
-    //     object-fit: cover;
-    // }
+                a {
+                    text-decoration: none;
+                }
+                .info_img {
+                    img {
+                        display: block;
+                        width: 100%;
+                        height: 100%;
+                        object-fit: cover;
+                    }
+                }
+                .inner_text {
+                    max-width: 280px;
+                    // height: 80px;
+                    font-size: $p1;
+                    line-height: 1.6;
+                    color: rgb(248, 248, 248);
+                }
+            }
+        }
+    }
+
+    //section9_boxButtom
+
+    .section9_boxButtom {
+        .unstore_title {
+            // border: 1px solid red;
+            color: $colorT;
+            padding: 80px 0;
+            &-c {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                img {
+                    width: 63px * 0.7;
+                    height: 43px * 0.7;
+                    margin-bottom: 5px;
+                }
+                h1 {
+                    font-size: $h1;
+                    padding: 5px 20px 10px;
+                }
+            }
+            &-e {
+                text-align: center;
+                font-size: $p1;
+            }
+        }
+
+        // ------ Swiper  ------
+        .index_swiper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 2%;
+            .index_prbtn2 {
+                margin-right: 15px;
+            }
+            .index_nextbtn2 {
+                margin-left: 15px;
+            }
+        }
+        .swiper-container {
+            width: 85%;
+            height: 100%;
+
+            .swiper-slide {
+                padding: 10px;
+                text-align: center;
+                font-size: 18px;
+
+                background-color: rgba(218, 199, 199, 0.6);
+                height: 400px;
+                width: 400px;
+                border: 5px solid rgb(65, 121, 173);
+                // border-radius: 55%;
+
+                /* Center slide text vertically */
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: -webkit-flex;
+                display: flex;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                -webkit-justify-content: center;
+                justify-content: center;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                -webkit-align-items: center;
+                align-items: center;
+                a {
+                    text-decoration: none;
+                }
+                .info_img {
+                    display: block;
+                    width: 300px;
+                    height: 285px;
+                }
+                .inner_text {
+                    // max-width: 280px;
+                    // height: 80px;
+                    padding-top: 20px;
+                    font-size: $p1;
+                    line-height: 1.6;
+                    text-align: center;
+                    color: rgb(248, 248, 248);
+                }
+            }
+        }
+    }
 }
 </style>
