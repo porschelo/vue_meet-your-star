9c0c4df1d71cfbfce9c727e8a23cc7740e638d93
gwalee <gwalee123@gmail.com>
2021-05-24 10:28:57 +0800

    搬家完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,12 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/home/Index.vue'; //component的自訂名稱
 import ProductDetail from '@/views/product_detail/Index.vue';
-
+import UnStore from '@/views/unstore/Index.vue';
+import Customized from '@/views/customized/Index.vue';
+import TestSort from '@/views/test_sort/Index.vue';
+import TestResult from '@/views/test_result/Index.vue';
+import TestQa from '@/views/test_qa/Index.vue';
+import Game from '@/views/game/Index.vue';
 
 // 動態載入（延遲）
 // route level code-splitting
@@ -22,6 +27,36 @@ const routes = [
         path: '/product_detail',
         name: 'ProductDetail',
         component: ProductDetail,
+    },
+    {
+        path: '/unstore',
+        name: 'UnStore',
+        component: UnStore,
+    },
+    {
+        path: '/customized',
+        name: 'Customized',
+        component: Customized,
+    },
+    {
+        path: '/test_sort',
+        name: 'TestSort',
+        component: TestSort,
+    },
+    {
+        path: '/test_result',
+        name: 'TestResult',
+        component: TestResult,
+    },
+    {
+        path: '/test_qa',
+        name: 'TestQa',
+        component: TestQa,
+    },
+    {
+        path: '/game',
+        name: 'Game',
+        component: Game,
     },
 ];
 
