991276c9439995f01a6ac8b2d102a2c11a665355
gwalee <gwalee123@gmail.com>
2021-06-03 08:50:44 +0800

    銘言修正

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,41 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
+    state: {
+
+        loginBol:false,
+        signBol:false,
+        loginStatus: 0 ,
+        memberIcon:"",
+        loginID:"",
+        
+    },
+    mutations: {
+
+        loginVisible(state,bol){
+            state.loginBol = bol;
+        },
+        signVisible(state,bol){
+            state.signBol = bol;
+        },
+        loginStatus(state,value){
+            state.loginStatus = value;
+        },
+        memberIcon(state,str){
+            state.memberIcon = str;
+        },
+        loginID(state,str){
+            state.loginID = str;
+        }
+
+    },
+    actions: {
+        setmemberIcon:function(context,str){
+            context.commit('memberIcon',str);
+        },
+        setloginID:function(context,loginID){
+            context.commit('loginID',loginID);
+        },
+    },
     modules: {},
 });
