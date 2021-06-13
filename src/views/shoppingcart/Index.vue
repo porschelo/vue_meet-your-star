f5abe82f748dec53c4f7d8ab38fed9cc711333c2
frankhsu519 <gary80519@yahoo.com.tw>
2021-06-13 23:31:26 +0800

    畫面統一位置

--- src/views/shoppingcart/Index.vue
+++ src/views/shoppingcart/Index.vue
@@ -12,7 +12,7 @@
                 </div>
 
                 <div class="SC_title-e">
-                    <h4>一 Shopping Cart 一</h4>
+                    <p>一 Shopping Cart 一</p>
                 </div>
             </div>
 
@@ -205,38 +205,50 @@ export default {
         // margin: 320*.7px 0 550*.7px 0;
         // border: $border red;
 
-        color: $colorT;
-        padding: (80px + 200px) * 0.7 0 0;
-
-        margin-bottom: 280px * 0.7;
-
-        &-c {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            img {
-                width: 63px * 0.7;
-                height: 43px * 0.7;
-                margin-bottom: 5px;
+        position: relative;
+            margin: auto;
+            color: $colorT;
+            padding: (80px+240px) * 0.7 0 0;
+            z-index: 15;
+            @include rwd(mobile) {
+                padding: (200px) * 0.7 0 0;
             }
-        }
-
-        h1 {
-            font-size: $h1;
-            padding: 5px 20px 10px;
-        }
-
-        &-e {
-            text-align: center;
-            font-size: $p1;
-        }
+            &-c {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                img {
+                    width: 63px * 0.7;
+                    height: 43px * 0.7;
+                    margin-bottom: 5px;
+                    @include rwd(mobile) {
+                        width: 63px * 0.5;
+                        height: 43px * 0.5;
+                    }
+                }
+            }
+            h1 {
+                font-size: $h1;
+                padding: 5px 20px 0px;
+                @include rwd(mobile) {
+                    font-size: $h1 - 8px;
+                    padding: 5px 15px 0px;
+                }
+            }
+            &-e {
+                text-align: center;
+                font-size: $p1;
+                @include rwd(mobile) {
+                    font-size: $p1 - 6px;
+                }
+            }
     }
 
     .SC_mid_block {
         // border: 1px solid red;
         max-width: 1300 * 0.7px;
         width: 100%;
+        padding-top: 50px;
 
         .list_info {
             border: 3px solid white;
