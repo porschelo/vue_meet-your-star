eec3077a9a3cd2fb1420645ddedd8c6a24ee5f9b
gwalee <gwalee123@gmail.com>
2021-06-14 22:07:59 +0800

    更改路徑

--- src/views/test_sort/Index.vue
+++ src/views/test_sort/Index.vue
@@ -132,10 +132,10 @@ export default {
             }
             h1 {
                 font-size: $h1;
-                padding: 5px 20px 0px;
+                padding: 5px 20px 5px;
                 @include rwd(mobile) {
                     font-size: $h1 - 8px;
-                    padding: 5px 15px 0px;
+                    padding: 5px 15px 5px;
                 }
             }
             &-e {
@@ -251,18 +251,5 @@ export default {
         background-color: #44796a;
     }
 
-    // .overlaymenu{
-    //     .menulist{
-    //         .nav-menu{
-    //             ul{
-    //                 li:nth-child(3){
-    //                     a{
-    //                         color: yellow;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
 }
 </style>
